0b837de9488ba4316381274a4ebf7ce1f482d237
nitic04 <nitic04@gmail.com>
2023-09-12 23:28:36 -0400

    Fixed sizing bug for about section image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
     <div className='w-full h-[100%] text-white bg-[#181818]'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='h-full w-full about flex flex-col md:flex-row'>
-          <div className='p-10 m:w-[130%]'>
+          <div className='p-10 lg:w-[130%]'>
               <Fade cascade damping={0.2} triggerOnce>
               <p className='text-4xl font-bold inline border-b-4 border-[#ED3833] pr-2'>Who We Are</p>
               <p className='pt-5'>The University of Toronto Formula Racing Team stands as an enterprising and driven student-led design collective hailing from the University of Toronto. Each year, the team undertakes the exhilarating challenge of crafting a compact, open-wheel race car from scratch.</p>
