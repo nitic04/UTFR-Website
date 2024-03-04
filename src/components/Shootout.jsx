6e106a2762a3e4c65cedaa536cb46cd0bf1ec402
Niti Chaudhary <nitic04@gmail.com>
2024-03-04 09:59:24 -0500

    Removed unused import

--- src/components/Shootout.jsx
+++ src/components/Shootout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import GoToTop from './GoToTop'
 import { Fade } from "react-awesome-reveal";
