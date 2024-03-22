7ee57a5cb68b946e510e12d83b07f0a45327eab5
Niti Chaudhary <nitic04@gmail.com>
2024-03-22 02:02:47 -0400

    Finished all the pages woooop finally

--- src/components/CarPages/UT21.jsx
+++ src/components/CarPages/UT21.jsx
@@ -2,18 +2,18 @@ import { Fade } from "react-awesome-reveal";
 import GoToTop from '../GoToTop'
 import HistoryCarousel from '../HistoryCarousel'
 
-import CarCutout from '../../assets/HistoricCarPhotos/UT'
-import OverviewImg from '../../assets/HistoricCarPhotos/UT'
-import CompImg1 from '../../assets/HistoricCarPhotos/UT'
-import CompImg2 from '../../assets/HistoricCarPhotos/UT'
-import CompImg3 from '../../assets/HistoricCarPhotos/UT'
-import CarouselImg1 from '../../assets/HistoricCarPhotos/UT'
-import CarouselImg2 from '../../assets/HistoricCarPhotos/UT'
-import CarouselImg3 from '../../assets/HistoricCarPhotos/UT'
-import CarouselImg4 from '../../assets/HistoricCarPhotos/UT'
-import CarouselImg5 from '../../assets/HistoricCarPhotos/UT'
-import CarouselImg6 from '../../assets/HistoricCarPhotos/UT'
-import CarouselImg7 from '../../assets/HistoricCarPhotos/UT'
+import CarCutout from '../../assets/HistoricCarPhotos/UT21/UT21_Cutout.png'
+import OverviewImg from '../../assets/HistoricCarPhotos/UT21/UT21.jpeg'
+import CompImg1 from '../../assets/HistoricCarPhotos/UT21/2021-Michigan-1.jpg'
+import CompImg2 from '../../assets/HistoricCarPhotos/UT21/2021-UTFR-Shootout-1.jpg'
+import CompImg3 from '../../assets/HistoricCarPhotos/UT21/UT21_2.jpg'
+import CarouselImg1 from '../../assets/HistoricCarPhotos/UT21/2021-Michigan-2.jpg'
+import CarouselImg2 from '../../assets/HistoricCarPhotos/UT21/2021-Testing-1.jpg'
+import CarouselImg3 from '../../assets/HistoricCarPhotos/UT21/2021-UTFR-Shootout-2.jpg'
+import CarouselImg4 from '../../assets/HistoricCarPhotos/UT21/2021-Pitt-Shootout-1.jpg'
+import CarouselImg5 from '../../assets/HistoricCarPhotos/UT21/UT21_3.jpg'
+import CarouselImg6 from '../../assets/HistoricCarPhotos/UT21/UT21_5.png'
+import TeamMembersSimpleComponents from "./TeamMembersSimpleComponent";
 
 const UT21 = () => {
     const slides = [
@@ -41,10 +41,20 @@ const UT21 = () => {
             image: CarouselImg6,
             description: ''
         },
-        {
-            image: CarouselImg7,
-            description: ''
-        },
+    ];
+    
+    const teamMembers = [
+        { name: 'Team Principal: ', specialization: 'Ben Sprenger' },
+        { name: 'Chassis: ', specialization: 'Jonathan Lee' },
+        { name: 'Suspension: ', specialization: 'Ben Sprenger' },
+        { name: 'Electrical: ', specialization: 'Daniel Wing & Kyle McCarroll' },
+        { name: 'Aerodynamics: ', specialization: 'Ally Fraser' },
+        { name: 'Drivetrain: ', specialization: 'Tejvir Binepal' },
+        { name: 'Engine: ', specialization: 'Areg Nazarins-Armavil & Kyle McCarroll' },
+        { name: 'Ergonomics: ', specialization: 'Megan Lee' },
+        { name: 'Brakes: ', specialization: 'Damian Sisera' },
+        { name: 'Business Lead: ', specialization: 'Jonathan Libby' },
+        { name: 'Outreach & Sponsorships: ', specialization: 'Juliano Sisera' },
     ];
 
   return (
@@ -52,20 +62,20 @@ const UT21 = () => {
         {/* Header */}
         <div className="h-screen bg-[#B6C1D0]">
             <Fade cascade damping={0.2} triggerOnce>
-            <h1 className="carTitle text-center text-[130px] md:text-[280px] font-bold text-white pt-[250px]">TITLE</h1>
+            <h1 className="carTitle text-center text-[130px] md:text-[280px] font-bold text-white pt-[250px]">UT21</h1>
             <img className="mx-auto pt-10 w-[1000px]" src={CarCutout} alt="" />
             <div className='grid space-y-8 md:space-y-0 md:grid-cols-3 pt-10 md:pt-5'>
                 <div>
-                    <h2 className="text-[#3A4254] text-center text-xl md:text-2xl font-bold">TITLE</h2>
-                    <h2 className="text-center text-white text-3xl md:text-4xl font-bold">SPEC</h2>
+                    <h2 className="text-[#3A4254] text-center text-xl md:text-2xl font-bold">Engine</h2>
+                    <h2 className="text-center text-white text-3xl md:text-4xl font-bold">Honda TRX Engine</h2>
                 </div>
                 <div>
-                    <h2 className="text-[#3A4254] text-center text-xl md:text-2xl font-bold">TITLE</h2>
-                    <h2 className="text-center text-white text-3xl md:text-4xl font-bold">SPEC</h2>
+                    <h2 className="text-[#3A4254] text-center text-xl md:text-2xl font-bold">Weight</h2>
+                    <h2 className="text-center text-white text-3xl md:text-4xl font-bold">370 lbs</h2>
                 </div>
                 <div>
-                    <h2 className="text-[#3A4254] text-center text-xl md:text-2xl font-bold">TITLE</h2>
-                    <h2 className="text-center text-white text-3xl md:text-4xl font-bold">SPEC</h2>
+                    <h2 className="text-[#3A4254] text-center text-xl md:text-2xl font-bold">Power</h2>
+                    <h2 className="text-center text-white text-3xl md:text-4xl font-bold">51 hp</h2>
                 </div>
             </div>
             </Fade>
@@ -76,7 +86,7 @@ const UT21 = () => {
             <div className='px-10 py-10 lg:px-24 lg:py-32'>
                 <Fade cascade damping={0.2} triggerOnce>
                 <h1 className='text-4xl md:text-6xl font-bold pb-10'>OVERVIEW</h1>
-                <p>PARAGRAPH</p>
+                <p>UT21 made significant strides in the 2020 UTFR season, achieving a commendable 19th place overall finish at Formula SAE Michigan.</p>
                 </Fade>
             </div>
             <div className='mx-auto my-auto px-10 pb-10 lg:pt-10 lg:pr-24'>
@@ -102,7 +112,7 @@ const UT21 = () => {
                     </div>
                     <Fade cascade damping={0.2} triggerOnce>
                     <h3 className='text-center text-2xl pt-5'>Formula SAE Michigan</h3>
-                    <h3 className='text-center pb-10 lg:pb-0 text-2xl font-bold'>5th Place Overall</h3>
+                    <h3 className='text-center pb-10 lg:pb-0 text-2xl font-bold'>19th Place Overall</h3>
                     <h3 className='hidden text-center text-2xl font-bold pb-10 lg:pb-0'>RESULT</h3>
                     </Fade>
                 </div>
@@ -116,8 +126,8 @@ const UT21 = () => {
                         }}>
                     </div>
                     <Fade cascade damping={0.2} triggerOnce>
-                    <h3 className='text-center text-2xl pt-5'>Formula Student UK</h3>
-                    <h3 className='text-center pb-10 lg:pb-0 text-2xl font-bold'>5th Place Overall</h3>
+                    <h3 className='text-center text-2xl pt-5 pb-10'>UTFR Shootout</h3>
+                    <h3 className='hidden text-center pb-10 lg:pb-0 text-2xl font-bold'>5th Place Overall</h3>
                     <h3 className='hidden text-center lg:pb-0 text-2xl font-bold'>RESULT</h3>
                     </Fade>
                 </div>
@@ -131,8 +141,8 @@ const UT21 = () => {
                         }}>
                     </div>
                     <Fade cascade damping={0.2} triggerOnce>
-                    <h3 className='text-center text-2xl pt-5'>Formula SAE East</h3>
-                    <h3 className='text-center lg:pb-0 text-2xl font-bold'>5th Place Overall</h3>
+                    <h3 className='text-center text-2xl pt-5 pb-10'>Pittsburgh Shootout</h3>
+                    <h3 className='hidden text-center lg:pb-0 text-2xl font-bold'>5th Place Overall</h3>
                     <h3 className='text-center lg:pb-0 text-2xl font-bold'>Efficiency - 1st Place</h3>
                     </Fade>
                 </div>
@@ -140,97 +150,8 @@ const UT21 = () => {
             </Fade>
         </div>
 
-        {/* Car Video Section */}
-        <div className='bg-[#13191F]'>
-            <Fade cascade damping={0.2} triggerOnce>
-            <h1 className='text-4xl md:text-6xl font-bold pt-10 pb-10 text-center text-white'>SEE THE YEAR IN ACTION</h1>
-            <iframe className="w-[100vw] h-[30vh] md:w-[80vw] md:h-[50vh] lg:w-[70vw] lg:h-[80vh] mx-auto pb-10" src="https://www.youtube.com/embed/ilPiE-aTMGQ?si=iaf0mMtAU3mIGKkK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </Fade>
-        </div>
-
         {/* Team Members Section */}
-        <div className='bg-[#303949] pb-10 px-10 text-[#222220]'>
-            <Fade cascade damping={0.2} triggerOnce>
-            <h1 className='text-5xl md:text-6xl font-bold pt-7 pb-10 text-center text-white'>UT06 TEAM MEMBERS</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-            
-            {/* Directors */}
-            <div className="teamCard bg-gray-100 p-4 rounded-lg py-7">
-                <Fade cascade damping={0.1} triggerOnce>
-                <h2 className="text-2xl font-semibold mb-2 pl-4">DIRECTORS</h2>
-                <ul className=''>
-                <li>Nadia Boin - Team Principal</li>
-                <li>Maggie Lafreniere - Technical Director</li>
-                </ul>
-                </Fade>
-            </div>
-
-            {/* Powertrain */}
-            <div className="teamCard bg-gray-100 p-4 rounded-lg py-7">
-                <Fade cascade damping={0.1} triggerOnce>
-                <h2 className="text-2xl font-semibold mb-2 pl-4">POWERTRAIN</h2>
-                <ul>
-                    <li>Nilufar Damji</li>
-                    <li>Antonio Gomes</li>
-                    <li>Leo Simonis</li>
-                    <li>Neal Persaud</li>
-                </ul>
-                </Fade>
-            </div>
-
-            {/* Drivetrain */}
-            <div className="teamCard bg-gray-100 p-4 rounded-lg py-7">
-                <Fade cascade damping={0.1} triggerOnce>
-                <h2 className="text-2xl font-semibold mb-2 pl-4">DRIVETRAIN</h2>
-                <ul className=''>
-                <li>Stefan Kloppenborg</li>
-                </ul>
-                </Fade>
-            </div>
-
-            {/* Suspension */}
-            <div className="teamCard bg-gray-100 p-4 rounded-lg py-7">
-                <Fade cascade damping={0.1} triggerOnce>
-                <h2 className="text-2xl font-semibold mb-2 pl-4">SUSPENSION</h2>
-                <ul>
-                    <li>Vince Libertucci</li>
-                    <li>Andrew Wong</li>
-                </ul>
-                </Fade>
-            </div>
-
-            {/* Chassis */}
-            <div className="teamCard bg-gray-100 p-4 rounded-lg py-7">
-                <Fade cascade damping={0.1} triggerOnce>
-                <h2 className="text-2xl font-semibold mb-2 pl-4">CHASSIS</h2>
-                <ul>        
-                    <li>Maggie Lafreniere</li>
-                </ul>
-                </Fade>
-            </div>
-
-            {/* Brakes */}
-            <div className="teamCard bg-gray-100 p-4 rounded-lg py-7">
-                <Fade cascade damping={0.1} triggerOnce>
-                <h2 className="text-2xl font-semibold mb-2 pl-4">BRAKES</h2>
-                <ul>        
-                    <li>Jason Kao</li>
-                </ul>
-                </Fade>
-            </div>
-
-            {/* Management */}
-            <div className="teamCard bg-gray-100 p-4 rounded-lg py-7">
-                <Fade cascade damping={0.1} triggerOnce>
-                <h2 className="text-2xl font-semibold mb-2 pl-4">MANAGEMENT</h2>
-                <ul>        
-                    <li>Daniel Zanini</li>
-                </ul>
-                </Fade>
-            </div>
-            </div>
-            </Fade>
-        </div>    
+        <TeamMembersSimpleComponents teamMembers={teamMembers} year={"1997"}/>
         
         {/* Photo Gallery Section */}
         <div className='bg-[#13191F] px-10 md:px-0'>
@@ -252,16 +173,16 @@ const UT21 = () => {
                 <div className='w-[850px] pb-3 px-5'>
                     <Fade cascade damping={0.1} triggerOnce>
                     <div className='flex border-b-2'>
-                        <p className='md:text-2xl py-5'>TITLE</p>
-                        <p className='md:text-2xl py-5 ml-auto'>SPEC</p>
+                        <p className='md:text-2xl py-5'>Engine</p>
+                        <p className='md:text-2xl py-5 ml-auto'>Honda TRX 450</p>
                     </div>
                     <div className='flex border-b-2'>
-                        <span className='hidden md:inline md:text-2xl py-5'>TITLE</span>
-                        <p className='md:text-2xl py-5 ml-auto'>SPEC</p>
+                        <span className='hidden md:inline md:text-2xl py-5'>Power</span>
+                        <p className='md:text-2xl py-5 ml-auto'>51 hp</p>
                     </div>
                     <div className='flex border-b-2'>
-                        <p className='md:text-2xl py-5'>TITLE</p>
-                        <p className='md:text-2xl py-5 ml-auto'>SPEC</p>
+                        <p className='md:text-2xl py-5'>Weight</p>
+                        <p className='md:text-2xl py-5 ml-auto'>375 lbs</p>
                     </div>
                     </Fade>
                 </div>
---
+++ src/components/CarPages/UT22.jsx
@@ -0,0 +1,149 @@
+import { Fade } from "react-awesome-reveal";
+import GoToTop from '../GoToTop'
+import HistoryCarousel from '../HistoryCarousel'
+
+import CarCutout from '../../assets/HistoricCarPhotos/UT22/UT22_Cutout.png'
+import OverviewImg from '../../assets/HistoricCarPhotos/UT22/UT22.jpeg'
+import CompImg1 from '../../assets/HistoricCarPhotos/UT22/UT22_Michigan-1.jpg'
+import CarouselImg1 from '../../assets/HistoricCarPhotos/UT22/UT22-Michigan-2.jpg'
+import CarouselImg2 from '../../assets/HistoricCarPhotos/UT22/UT22_4.png'
+import CarouselImg3 from '../../assets/HistoricCarPhotos/UT22/UT22_5.png'
+import CarouselImg4 from '../../assets/HistoricCarPhotos/UT22/UT22_6.png'
+import CarouselImg5 from '../../assets/HistoricCarPhotos/UT22/UT22_7.png'
+
+const UT22 = () => {
+    const slides = [
+        {
+            image: CarouselImg1,
+            description: ''
+        },
+        {
+            image: CarouselImg2,
+            description: ''
+        },
+        {
+            image: CarouselImg3,
+            description: ''
+        },
+        {
+            image: CarouselImg4,
+            description: ''
+        },
+        {
+            image: CarouselImg5,
+            description: ''
+        },
+    ];
+
+  return (
+    <div>
+        {/* Header */}
+        <div className="h-screen bg-[#B6C1D0]">
+            <Fade cascade damping={0.2} triggerOnce>
+            <h1 className="carTitle text-center text-[130px] md:text-[280px] font-bold text-white pt-[250px]">UT21</h1>
+            <img className="mx-auto pt-10 w-[1000px]" src={CarCutout} alt="" />
+            <div className='grid space-y-8 md:space-y-0 md:grid-cols-3 pt-10 md:pt-5'>
+                <div>
+                    <h2 className="text-[#3A4254] text-center text-xl md:text-2xl font-bold">Motor</h2>
+                    <h2 className="text-center text-white text-3xl md:text-4xl font-bold">EMRAX 228</h2>
+                </div>
+                <div>
+                    <h2 className="text-[#3A4254] text-center text-xl md:text-2xl font-bold">Power</h2>
+                    <h2 className="text-center text-white text-3xl md:text-4xl font-bold">110 hp (80 kW)</h2>
+                </div>
+                <div>
+                    <h2 className="text-[#3A4254] text-center text-xl md:text-2xl font-bold">Top Speed</h2>
+                    <h2 className="text-center text-white text-3xl md:text-4xl font-bold">116 km/h</h2>
+                </div>
+            </div>
+            </Fade>
+        </div>
+
+        {/* Overview Section */}
+        <div className='bg-[#303949] grid lg:grid-cols-2 text-white'>
+            <div className='px-10 py-10 lg:px-24 lg:py-32'>
+                <Fade cascade damping={0.2} triggerOnce>
+                <h1 className='text-4xl md:text-6xl font-bold pb-10'>OVERVIEW</h1>
+                <p>In 2022, UTFR embarked on a significant transition as they shifted their focus to electric vehicles. The steel tube frame chassis, complemented by double unequal wishbone suspension, demonstrates the team's commitment to structural integrity and handling performance. However, the transition to electric power proved to be a formidable undertaking, with the team encountering technical hurdles that prevented the car from running at all during the competition year. However utilizing simulation software, the team was able to assess the performance of the car. Despite these setbacks, the team showcased remarkable resilience and adaptability in the face of adversity. The team's extensive roster of members, consisting of a diverse array of talents and expertise, underscored their commitment to collaboration and teamwork. Notably, the year was marked by significant disruptions due to the COVID-19 pandemic, with two shutdowns and strict safety measures impacting team operations and sponsorships. Nevertheless, the team persevered, laying the groundwork for future success and establishing crucial partnerships, including the beginning of a sponsorship with Tesla. Despite the challenges faced, UTFR's journey in 2022 serves as a testament to their unwavering dedication to pushing the boundaries of electric vehicle technology in the realm of Formula SAE competitions.</p>
+                </Fade>
+            </div>
+            <div className='mx-auto my-auto px-10 pb-10 lg:pt-10 lg:pr-24'>
+                <Fade cascade damping={0.2} triggerOnce>
+                <img src={OverviewImg} alt="" />
+                </Fade>
+            </div>
+        </div>
+
+        {/* Competitions Section */}
+        <div className='bg-[#062644] text-white'>
+            <Fade cascade damping={0.2} triggerOnce>
+            <h1 className='text-4xl md:text-6xl font-bold py-10 text-center'>COMPETITIONS</h1>
+            <div className='grid lg:grid-cols-3 items-center pb-10'>
+                <div className='px-10'>
+                    <div className='competitionCard h-[400px]' style={{
+                        backgroundImage: `url(${CompImg1})`,
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPositionY: "10%"
+                        }}>
+                    </div>
+                    <Fade cascade damping={0.2} triggerOnce>
+                    <h3 className='text-center text-2xl pt-5'>Formula SAE Michigan</h3>
+                    <h3 className='text-center pb-10 lg:pb-0 text-2xl font-bold'>21st Place Overall</h3>
+                    <h3 className='hidden text-center text-2xl font-bold pb-10 lg:pb-0'>RESULT</h3>
+                    </Fade>
+                </div>
+            </div>
+            </Fade>
+        </div>
+
+        {/* Photo Gallery Section */}
+        <div className='bg-[#13191F] px-10 md:px-0'>
+            <div className=''>
+            <Fade cascade damping={0.2} triggerOnce>
+                <h1 className='text-4xl text-white pt-10 text-center md:text-6xl pb-5 flex justify-center font-bold'>PHOTO GALLERY</h1>
+                <div className='lg:px-52 pb-5'>
+                    <HistoryCarousel slides={slides}/>
+                </div>
+            </Fade>
+            </div>
+        </div>
+
+        {/* Technical Specifications Section */}
+        <div className=' bg-[#062644]'>
+            <Fade cascade damping={0.2} triggerOnce>
+            <h1 className='text-4xl text-white pt-10 text-center md:text-6xl pb-5 flex justify-center font-bold'>TECHNICAL SPECIFICATIONS</h1>
+            <div className='flex text-white justify-center pb-10'>
+                <div className='w-[850px] pb-3 px-5'>
+                    <Fade cascade damping={0.1} triggerOnce>
+                    <div className='flex border-b-2'>
+                        <p className='md:text-2xl py-5'>Motor</p>
+                        <p className='md:text-2xl py-5 ml-auto'>EMRAX 228</p>
+                    </div>
+                    <div className='flex border-b-2'>
+                        <span className='hidden md:inline md:text-2xl py-5'>Peak Voltage</span>
+                        <p className='md:text-2xl py-5 ml-auto'>546 V</p>
+                    </div>
+                    <div className='flex border-b-2'>
+                        <p className='md:text-2xl py-5'>Power</p>
+                        <p className='md:text-2xl py-5 ml-auto'>110 hp (80 kW)</p>
+                    </div>
+                    <div className='flex border-b-2'>
+                        <p className='md:text-2xl py-5'>Top Speed</p>
+                        <p className='md:text-2xl py-5 ml-auto'>116 km/h</p>
+                    </div>
+                    <div className='flex border-b-2'>
+                        <p className='md:text-2xl py-5'>Weight</p>
+                        <p className='md:text-2xl py-5 ml-auto'>536 lbs</p>
+                    </div>
+                    </Fade>
+                </div>
+            </div>
+            </Fade>
+        </div>
+        <GoToTop />
+    </div>
+  )
+}
+export default UT22
