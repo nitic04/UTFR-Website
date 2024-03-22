3fc25d4ed1ee02df2832ca8586ed613e2042993d
Niti Chaudhary <nitic04@gmail.com>
2024-03-22 11:36:09 -0400

    I'm confused again

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -150,7 +150,7 @@ const History = () => {
               <Fade cascade damping={0.2} triggerOnce>
               <h2 className="text-5xl pl-8 pt-10 font-bold">Team History</h2>
               <p className="px-8 py-5">The University of Toronto Formula SAE Racing Team was founded during the 1996-97 academic year. Leading up to our first competition season in 1999 the team was busy finding sponsors, conducting R&D and developing a prototype vehicle. In 2003, 2005, and 2006, the team took three overall championships at Formula Student UK - a record that would stand until 2016! We also placed in the top 5 in 2002, 2004 and 2007.</p>
-              <p className="px-8 pb-10">Over the course of the 2000s, several different chassis concepts were explored, moving from steel space frames to hybrid concepts, and finally to a full carbon fibre monocoque by the end of the decade. Now our team has transitioned from combustion vehicles to fully electric marking yet another era in UTFR's legacy.</p>
+              <p className="px-8 pb-10">Over the course of the 2000s, several different chassis concepts were explored, moving from steel space frames to hybrid concepts, and finally to a full carbon fibre monocoque by the end of the decade. Now our team has transitioned from combustion vehicles to fully electric marking yet another era in UTFR's legacy.</p> 
               </Fade>
             </div>
           </div>
