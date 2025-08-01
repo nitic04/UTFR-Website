7e8b614ca00c24fa9cfc4711ea08f915caea4727
Niti Chaudhary <nitic04@gmail.com>
2025-07-31 21:39:51 -0400

    Removed Batemo and added ESAB

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -7,12 +7,6 @@ const urlEndpoint = 'https://ik.imagekit.io/utfroutreach/'
 
 const diamondSponsors = [
     {
-        name: "Batemo",
-        imageUrl: urlEndpoint + "Diamond/Batemo.png",
-        desc: "Batemo is the global industry leader for the creation of lithium-ion battery simulation software. They kindly provide our team with battery simulators. For more information about the incredible work they do, please see their website.",
-        url: "http://www.batemo.de/"
-    },
-    {
         name: "RLXSolutions",
         imageUrl: urlEndpoint + "Gold/RLX_Logo.png?updatedAt=1734039962970",
         desc: "RLX Solutions has generously supported UTFR through the manufacturing of the many custom Printed Circuit Boards (PCBs) that go on the car. The resulting high quality PCBs can surmount the rigorous racing conditions when delivering power, reading sensor data, managing our high voltage battery pack, and ensuring the car operates safely. Their generous support keeps the electrical systems on the car running year after year, and has resulted in top finishes at all competitions we've competed at.",
@@ -121,6 +115,10 @@ const silverSponsors = [
     {
         imageUrl: urlEndpoint + "Silver/Ensoft.png",
         url: "https://www.ensoftcorp.com"
+    },
+    {
+        imageUrl: urlEndpoint + "Silver/ESAB.png",
+        url: "https://esab.com/ca/nam_en/"
     },
     {
         imageUrl: urlEndpoint + "Silver/FacFox.png",
@@ -483,7 +481,7 @@ const Sponsors = () => {
         {/* Diamond Sponsors */}
         <h1 className='flex justify-center bg-[#e3e3e3] pt-10 text-5xl font-bold text-center'>Diamond Sponsors</h1>
         <div className='flex justify-center bg-[#e3e3e3]'>
-            <div className="grid lg:grid-cols-3 md:grid-cols-1 py-10 gap-x-8 gap-y-10">
+            <div className="grid lg:grid-cols-2 md:grid-cols-1 py-10 gap-x-8 gap-y-10">
                 {diamondSponsors.map((sponsor, index) => (
                     <Fade delay={0.2} triggerOnce>
                         <DiamondSponsorCard
